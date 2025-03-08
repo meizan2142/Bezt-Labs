0a21d4acd9a0218aa0413199bccfc88826ab7962
Your Name01~git <saifsahai2002@gmail.com>
2025-03-09 00:13:08 +0600

    Background animation is implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import StarBackGround from "./StarBackground/StarBackGround"
+import StarBackGround from "./StarBackground/StarBackGround";
 
 function App() {
-
   return (
-    <div className='bg-[#014556] min-h-screen text-center'>
-      <div className="bg-red-500 grid grid-cols-1 text-center space-y-96 w-[380px] h-screen main-div mx-auto">
-        <StarBackGround/>
-        {/* Top part */}
-        <div>
-          {/* Logo */}
-          <div>
+    <div className="bg-[#014556] min-h-screen text-center flex justify-center items-center">
+      <div className="relative bg-red-500 grid grid-cols-1 text-center space-y-96 w-[380px] h-screen main-div">
+        {/* Star background */}
+        <StarBackGround className="absolute inset-0 w-full h-full" />
 
-          </div>
+        {/* Top part */}
+        <div className="relative z-10">
+          {/* Logo */}
+          <div></div>
           {/* Text */}
           <div className="space-y-2">
             <h1 className="text-white font-bold text-2xl">Welcome to Abouv</h1>
-            <p className="text-white text-base font-normal">Assesments that get you hired</p>
+            <p className="text-white text-base font-normal">
+              Assessments that get you hired
+            </p>
           </div>
           {/* 3 Logos */}
           <div>
@@ -24,20 +25,25 @@ function App() {
             <img src="" alt="" />
           </div>
         </div>
+
         {/* Bottom Part */}
-        <div className="">
+        <div className="relative z-10">
           <h1>Login or Signup</h1>
           {/* Input fields */}
           <div className="grid grid-cols-1 justify-center items-center">
-            <input type="number" name="" id="" className=" w-1/2 mx-auto"/>
-            <button className="bg-[#022730] text-white font-bold w-1/2 mx-auto">Continue</button>
+            <input type="number" className="w-1/2 mx-auto" />
+            <button className="bg-[#022730] text-white font-bold w-1/2 mx-auto">
+              Continue
+            </button>
           </div>
           {/* Terms & Conditions */}
-          <p><span className="text-base">By continuing, you agree to our</span></p>
+          <p>
+            <span className="text-base">By continuing, you agree to our</span>
+          </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/StarBackground/StarBackground.jsx
+++ src/StarBackground/StarBackground.jsx
@@ -6,14 +6,15 @@ const StarBackGround = () => {
                     key={i}
                     className="star"
                     style={{
-                        top: `${Math.random() * 100}%`,
-                        left: `${Math.random() * 100}%`,
-                        animationDelay: `${Math.random() * 5}s`,
+                        top: `${Math.random() * 100}vh`, // Start from a random vertical position
+                        left: `${Math.random() * 100}vw`, // Random horizontal position
+                        animationDuration: `${Math.random() * 10 + 5}s`, // Random duration between 5s - 15s
+                        animationDelay: `${Math.random() * 5}s`, // Random delay up to 5s
                     }}
                 ></div>
             ))}
         </div>
-    )
-}
+    );
+};
 
-export default StarBackGround
+export default StarBackGround;
