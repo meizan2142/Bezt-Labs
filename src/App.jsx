5da1c5a0a8d2d8554a70dd0c675a6d7ec930266d
Your Name01~git <saifsahai2002@gmail.com>
2025-03-10 02:29:13 +0600

    login page created succesfully

--- src/App.jsx
+++ src/App.jsx
@@ -3,52 +3,58 @@ import StarBackGround from "./StarBackground/StarBackGround";
 
 function App() {
   return (
-    <div className="bg-[#014556] min-h-screen text-center flex justify-center items-center">
-      <div className="relative bg-red-500 grid grid-cols-1 text-center space-y-96 w-[380px] h-screen main-div">
-        {/* Star background */}
+    <div className="bg-[#014556] min-h-screen flex justify-center items-center text-center">
+      <div className="relative w-[380px] h-screen flex flex-col justify-between items-center py-10 px-6">
+        {/* Star Background */}
         <StarBackGround className="absolute inset-0 w-full h-full" />
 
-        {/* Top part */}
-        <div className="relative z-10">
+        {/* Top Section */}
+        <div className="relative z-10 flex flex-col items-center space-y-4">
           {/* Logo */}
-          <div className="grid items-center justify-center">
-            <div className="w-20 h-20 rounded-full border outline-none grid items-center justify-center bg-black relative overflow-hidden">
-              <div className="absolute top-0 left-0 w-full h-1/3 bg-[#006680]" />
-              <div className="absolute top-1/3 left-0 w-full h-1/3 bg-[#298F96]" />
-              <div className="absolute bottom-0 left-0 w-full h-1/3 bg-[#37C8AB]" />
-              <div className="w-12 h-12 rounded-full border outline-none grid items-center justify-center bg-white relative z-10">
-              <FaAngleUp size={29} className="text-black"/>
-              </div>
+          <div className="w-20 h-20 rounded-full border grid place-items-center bg-black relative overflow-hidden">
+            <div className="absolute top-0 left-0 w-full h-1/3 bg-[#006680]" />
+            <div className="absolute top-1/3 left-0 w-full h-1/3 bg-[#298F96]" />
+            <div className="absolute bottom-0 left-0 w-full h-1/3 bg-[#37C8AB]" />
+            <div className="w-12 h-12 rounded-full border bg-white grid place-items-center z-10">
+              <FaAngleUp size={29} />
             </div>
           </div>
-          {/* Text */}
-          <div className="space-y-2">
-            <h1 className="text-white font-bold text-2xl">Welcome to Abouv</h1>
-            <p className="text-white text-base font-normal">
-              Assessments that get you hired
-            </p>
-          </div>
-          {/* 3 Logos */}
-          <div>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+
+          {/* Title & Subtitle */}
+          <h1 className="text-white font-bold text-2xl">Welcome to Abouv</h1>
+          <p className="text-white text-base font-normal">
+            Assessments that get you hired
+          </p>
+
+          {/* Three Icons */}
+          <div className="flex gap-6 mt-2">
+            <img src="/icons/icon1.png" alt="Icon 1" className="w-12 h-12" />
+            <img src="/icons/icon2.png" alt="Icon 2" className="w-12 h-12" />
+            <img src="/icons/icon3.png" alt="Icon 3" className="w-12 h-12" />
           </div>
         </div>
 
-        {/* Bottom Part */}
-        <div className="relative z-10">
-          <h1>Login or Signup</h1>
-          {/* Input fields */}
-          <div className="grid grid-cols-1 justify-center items-center">
-            <input type="number" className="w-1/2 mx-auto" />
-            <button className="bg-[#022730] text-white font-bold w-1/2 mx-auto">
+        {/* Bottom Section */}
+        <div className="relative z-10 w-full text-center space-y-4">
+          <h2 className="text-white font-semibold text-lg">Login or Signup</h2>
+
+          {/* Input & Button */}
+          <div className="space-y-3">
+            <input
+              type="number"
+              className="w-full p-3 bg-white rounded-md outline-none"
+              placeholder="10 digit mobile number"
+            />
+            <button className="w-full bg-[#022730] text-white font-bold p-3 rounded-md">
               Continue
             </button>
           </div>
+
           {/* Terms & Conditions */}
-          <p>
-            <span className="text-base">By continuing, you agree to our</span>
+          <p className="text-white text-sm">
+            By continuing, you agree to our <br />
+            <a href="#" className="underline font-bold">Terms & Conditions</a> &nbsp;
+            <a href="#" className="underline font-bold">Privacy Policy</a>
           </p>
         </div>
       </div>
